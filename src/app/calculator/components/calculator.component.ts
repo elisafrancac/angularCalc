bdca1efb91fceb887a916c06375bdfead3b2dff8
Elisa <elisafranca.carvalho@gmail.com>
2021-10-02 22:41:26 -0300

    feat: implements CalculatorComponent

--- src/app/calculator/components/calculator.component.ts
+++ src/app/calculator/components/calculator.component.ts
@@ -9,9 +9,85 @@ import { CalculatorService } from '../services';
 })
 export class CalculatorComponent implements OnInit {
 
+  private number1: string;
+  private number2: string;
+  private result: number;
+  private operation: string;
+
   constructor(private calculatorService: CalculatorService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  	this.clean();
+  }
+
+  clean(): void {
+  	this.number1 = '0';
+  	this.number2 = null;
+  	this.result = null;
+  	this.operation = null;
+  }
+
+  addNumber(number: string): void {
+  	if (this.operation === null) {
+  	  this.number1 = this.concatNumber(this.number1, number);
+  	} else {
+  	  this.number2 = this.concatNumber(this.number2, number);
+  	}
+  }
+
+  concatNumber(currNumber: string, numConcat: string): string {
+    if (currNumber === '0' || currNumber === null) {
+  	  currNumber = '';
+  	}
+
+  	if (numConcat === '.' && currNumber === '') {
+  	  return '0.';
+  	}
+
+  	if (numConcat === '.' && currNumber.indexOf('.') > -1) {
+  	  return currNumber;
+  	}
+
+  	return currNumber + numConcat;
+  }
+
+  defineOperation(operation: string): void {
+  	if (this.operation === null) {
+      this.operation = operation;
+      return;
+  	}
+
+  	if (this.number2 !== null) {
+  		this.result = this.calculatorService.calc(
+  			parseFloat(this.number1),
+  			parseFloat(this.number2),
+  			this.operation);
+  		this.operation = operation;
+  		this.number1 = this.result.toString();
+  		this.number2 = null;
+  		this.result = null;
+  	}
+  }
+
+  calc(): void {
+  	if (this.number2 === null) {
+  		return;
+  	}
+
+  	this.result = this.calculatorService.calc(
+  		parseFloat(this.number1),
+  		parseFloat(this.number2),
+  		this.operation);
+  }
+
+  get display(): string {
+  	if (this.result !== null) {
+  		return this.result.toString();
+  	}
+  	if (this.number2 !== null) {
+  		return this.number2;
+  	}
+  	return this.number1;
   }
 
 }
