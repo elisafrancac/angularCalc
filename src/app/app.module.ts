40279d141f36a1c28687a110352973b1115abe55
Elisa <elisafranca.carvalho@gmail.com>
2021-10-02 22:51:00 -0300

    add font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CalculatorModule } from './calculator';
 
+// Estudos de Angular, exercícios criados com base no curso:
+// Formação Angular 12 - O início criando 7 projetos (Udemy)
+
 @NgModule({
   declarations: [
     AppComponent
